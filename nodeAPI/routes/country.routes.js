b6666e0c34d0011975157f3e0e31aa7ead94e3c1
divyatadvi12 <divyatadvi689@gmail.com>
2026-02-11 12:41:28 +0530

    Update country.routes.js

--- nodeAPI/routes/country.routes.js
+++ nodeAPI/routes/country.routes.js
@@ -8,6 +8,8 @@ router.get('/',cnController.getAllcn)
 router.get('/:id',cnController.getCnById)
 router.post('/',cnController.insertCn)
 router.put('/:id',cnController.updateCn)
+
+  
 router.delete('/:id',cnController.removeCn)
 
 module.exports=router
